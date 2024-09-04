44943af78740076f669db4d0f52a0ce1691cc45f
rawanlaham <rawan2001allaham@gmail.com>
2024-09-04 22:36:52 +0300

    change the register  server

--- project1v5/lib/project_materials/constants/linkapi.dart
+++ project1v5/lib/project_materials/constants/linkapi.dart
@@ -1,7 +1,7 @@
 const String linkServerName = "http://0.0.0.0";
 
 // Auth
-const String linkSignUp = "http://10.0.2.2/api/register";
+const String linkSignUp = "http://10.0.2.2:8000/api/register";
 
 //"$linkServerName/auth/signup";
 
